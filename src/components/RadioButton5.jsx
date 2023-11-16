8f5c3f2f65a042e7caa8e543d455100302222ad4
Freja Fischer <frej657e@stud.kea.dk>
2023-11-16 12:52:01 +0100

    first commit, setup with 5 radio

---
+++ src/components/RadioButton5.jsx
@@ -0,0 +1,105 @@
+import styles from "./RadioButton.module.css";
+
+export default function RadioButton5({ text1, text2, text3, text4, text5 }) {
+  return (
+    <>
+      <div className="flex flex-col pl-5 pt-3 mb-10">
+        <div className={`${styles.radioinput}`}>
+          <input value={text1} name="color" id="color-1" type="radio" />
+          <label className="flex" htmlFor="color-1">
+            <span className={styles.svg}>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                <g strokeWidth="0" id="SVGRepo_bgCarrier"></g>
+                <g strokeLinejoin="round" strokeLinecap="round" id="SVGRepo_tracerCarrier"></g>
+                <g id="SVGRepo_iconCarrier">
+                  {" "}
+                  <g id="Interface / Check">
+                    {" "}
+                    <path strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" stroke="#005B44" d="M6 12L10.2426 16.2426L18.727 7.75732" id="Vector"></path>{" "}
+                  </g>{" "}
+                </g>
+              </svg>
+            </span>
+            <div className="self-center ml-8">{text1}</div>
+          </label>
+        </div>
+        <div className={`${styles.radioinput}`}>
+          <input value={text2} name="color" id="color-2" type="radio" />
+          <label className="flex" htmlFor="color-2">
+            <span className={styles.svg}>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                <g strokeWidth="0" id="SVGRepo_bgCarrier"></g>
+                <g strokeLinejoin="round" strokeLinecap="round" id="SVGRepo_tracerCarrier"></g>
+                <g id="SVGRepo_iconCarrier">
+                  {" "}
+                  <g id="Interface / Check">
+                    {" "}
+                    <path strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" stroke="#005B44" d="M6 12L10.2426 16.2426L18.727 7.75732" id="Vector"></path>{" "}
+                  </g>{" "}
+                </g>
+              </svg>
+            </span>
+            <div className="self-center ml-8">{text2}</div>
+          </label>
+        </div>
+        <div className={`${styles.radioinput}`}>
+          <input value={text3} name="color" id="color-3" type="radio" />
+          <label className="flex" htmlFor="color-3">
+            <span className={styles.svg}>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                <g strokeWidth="0" id="SVGRepo_bgCarrier"></g>
+                <g strokeLinejoin="round" strokeLinecap="round" id="SVGRepo_tracerCarrier"></g>
+                <g id="SVGRepo_iconCarrier">
+                  {" "}
+                  <g id="Interface / Check">
+                    {" "}
+                    <path strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" stroke="#005B44" d="M6 12L10.2426 16.2426L18.727 7.75732" id="Vector"></path>{" "}
+                  </g>{" "}
+                </g>
+              </svg>
+            </span>
+            <div className="self-center ml-8">{text3}</div>
+          </label>
+        </div>
+        <div className={`${styles.radioinput}`}>
+          <input value={text4} name="color" id="color-3" type="radio" />
+          <label className="flex" htmlFor="color-3">
+            <span className={styles.svg}>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                <g strokeWidth="0" id="SVGRepo_bgCarrier"></g>
+                <g strokeLinejoin="round" strokeLinecap="round" id="SVGRepo_tracerCarrier"></g>
+                <g id="SVGRepo_iconCarrier">
+                  {" "}
+                  <g id="Interface / Check">
+                    {" "}
+                    <path strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" stroke="#005B44" d="M6 12L10.2426 16.2426L18.727 7.75732" id="Vector"></path>{" "}
+                  </g>{" "}
+                </g>
+              </svg>
+            </span>
+            <div className="self-center ml-8">{text4}</div>
+          </label>
+        </div>
+        <div className={`${styles.radioinput}`}>
+          <input value={text5} name="color" id="color-3" type="radio" />
+          <label className="flex" htmlFor="color-3">
+            <span className={styles.svg}>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                <g strokeWidth="0" id="SVGRepo_bgCarrier"></g>
+                <g strokeLinejoin="round" strokeLinecap="round" id="SVGRepo_tracerCarrier"></g>
+                <g id="SVGRepo_iconCarrier">
+                  {" "}
+                  <g id="Interface / Check">
+                    {" "}
+                    <path strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" stroke="#005B44" d="M6 12L10.2426 16.2426L18.727 7.75732" id="Vector"></path>{" "}
+                  </g>{" "}
+                </g>
+              </svg>
+            </span>
+            <div className="self-center ml-8">{text5}</div>
+          </label>
+        </div>
+      </div>
+    </>
+  );
+}
