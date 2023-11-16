15b292b299bf6d9bd18dc8202c1832ada76f0749
Julie <julieeriksen09@hotmail.com>
2023-11-16 09:39:41 +0100

    Ændret font-størrelse i productcard

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -4,7 +4,7 @@ export default function ProductCard() {
     <article className="border-2 rounded-lg p-4 m-2">
       <Image src="https://images.matas.dk/trs/w730//encode/3614272761421_20231012141727.jpg" width={500} height={500} alt="Picture of the author"></Image>
       <h4 className="font-semibold text-sm mt-2">Valentino</h4>
-      <h5 className="text-[#6A6A6A] font-medium text-sm">Donna Born in..</h5>
+      <h5 className="text-[#6A6A6A] font-medium text-xs">Donna Born in Roma Eau de Parfum</h5>
       <p className="font-semibold mt-3">624,95 kr.</p>
     </article>
   );
