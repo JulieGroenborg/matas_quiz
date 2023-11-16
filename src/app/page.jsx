7cf3adfae4eed7c8d5b1349deaf5d2bd87468252
Julie <julieeriksen09@hotmail.com>
2023-11-16 19:53:03 +0100

    Working on values from child to parent

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -13,6 +13,7 @@ export default function Home() {
   //metode til at få vist "flere sider" ligesom i miniquizzen
   const [visible, setVisible] = useState(1);
   const [stepCount, setStepCount] = useState(0);
+  const [details, setDetails] = useState();
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-4 bg-gray-300">
       <QuizLayout currentStep={stepCount}>
@@ -64,14 +65,16 @@ export default function Home() {
         {visible === 4 && (
           <section>
             <h3>Kender du gavemodtagerens præferencer?</h3>
-            <SquareRadioButtons></SquareRadioButtons>
+            <SquareRadioButtons onChange={(value) => setDetails(value)}></SquareRadioButtons>
 
-            <RadioButton3></RadioButton3>
-
+            <div className="hidden">
+              <RadioButton3></RadioButton3>
+            </div>
             <PrimaryButton
               text="Vis resultat"
               action={() => {
                 setVisible((o) => o + 1);
+                onChange = { answer };
               }}
             />
           </section>
--- src/components/SquareRadioButtons.jsx
+++ src/components/SquareRadioButtons.jsx
@@ -2,21 +2,21 @@ import styles from "./SquareRadioButtons.module.css";
 import { useState } from "react";
 
 export default function SquareRadioButtons() {
-  const [selectedValue, setSelectedValue] = useState(null);
+  const [value, setValue] = useState(null);
 
   function onChange(event) {
-    setSelectedValue(event.target.value);
+    setValue(event.target.value);
     console.log(event.target.value);
   }
 
   return (
     <form className="flex pt-8 mb-14 place-content-center">
       <label className={`${styles.label}`}>
-        <input onChange={onChange} checked={selectedValue === "true"} value="true" className={styles.input} type="radio" name="select" />
+        <input onChange={onChange} checked={value === "true"} value="true" className={styles.input} type="radio" name="select" />
         <span className={styles.span}>Ja</span>
       </label>
       <label className={`${styles.label} ml-5`}>
-        <input onChange={onChange} checked={selectedValue === "false"} value="false" className={styles.input} type="radio" name="select" />
+        <input onChange={onChange} checked={value === "false"} value="false" className={styles.input} type="radio" name="select" />
         <span className={styles.span}>Nej</span>
       </label>
     </form>
