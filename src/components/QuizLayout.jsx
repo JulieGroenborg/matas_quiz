0352a2895c58a477e5c0d6d65a5f77dfb7fea20d
Julie <julieeriksen09@hotmail.com>
2023-11-23 12:36:39 +0100

    placeret stepper

--- src/components/QuizLayout.jsx
+++ src/components/QuizLayout.jsx
@@ -34,7 +34,7 @@ export default function QuizLayout({ children, currentStep }) {
     <div className="w-full flex flex-col place-content-center content-center bg-white rounded md:w-2/4">
       <header className="flex flex-col justify-items-center items-center">
         <div className={style.container}>
-          <Image src={giftImage} alt="Gifts" width="fill" height="fill" priority className={style.image} />
+          <Image src={giftImage} alt="Gifts" width="fill" height="fill" priority className={`${style.image} pb-6`} />
           <svg className={style.close} xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="#FFFFFF" viewBox="0 0 16 16">
             <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
             <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
